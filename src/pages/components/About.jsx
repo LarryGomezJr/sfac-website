0376ea390d29d46c623b2c53fe4166fe928a0390
Cassey <lontoccassey@gmail.com>
2024-11-05 20:03:35 +0800

    About

---
+++ src/pages/components/About.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section class="text-gray-400 bg-gray-900 body-font">
+  <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+    <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+      <img class="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600"/>
+    </div>
+    <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">ABOUT OUR
+      SCHOOL</h1>
+      <p class="mb-8 leading-relaxed">Saint Francis of Assisi College had its beginnings in 1981, when the husband-and-wife team of Dr. Evangeline Oñas-Orosco and Dr. Arturo Agapito Orosco, Sr., opened a two-classroom preparatory school in Talon III, Las Piñas City to provide affordable quality education for young children. By 1983, the school’s popularity was such that a two-storey, full elementary school building had to be constructed to meet the needs of the surrounding community. The opening of the high school department quickly followed in 1986, ensuring placements for SFAC graduates at the secondary level. At the same time, two new campuses were established, one in Taguig City, and one in Los Baños, Laguna.
+
+In 1989, the tertiary level in SFAC came into being offering several courses both in degree and non-degree programs. New campuses have since been added: the Pamplona, Las Piñas campus in 1990, the Bacoor, Cavite campus in 1994, the Alabang, Muntinlupa campus in 1996. In the same year, 1996, Saint Anthony School of Las Piñas came under SFAC supervision and management. Dasmariñas, Cavite campus opened in 1999. The Biñan campus, in Laguna opened in 2006. In 2010, the Los Baños campus was renamed Saint Anthony School of Los Baños, named after the patron saint of the locality. The 10th campus, located in Sta. Rosa, Laguna accepts enrollees starting S.Y. 2012 – 2013. In line with corporate branding moves, the Pamplona campus, being the smallest campus, moved its operation to the main campus in 2014..</p>
+      <div class="flex w-full md:justify-start justify-center items-end">
+        
+        
+      </div>
+      
+    </div>
+  </div>
+</section>
+  )
+}
+
+export default About
