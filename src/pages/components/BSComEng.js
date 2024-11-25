4c9a98d06fb96a92cefd7f2032950a6c1442d3a1
nitchiii <148184975+nitchiii@users.noreply.github.com>
2024-11-25 20:07:12 +0800

    BS Computer Engineering Component

---
+++ src/pages/components/BSComEng.js
@@ -0,0 +1,122 @@
+import React from 'react'
+import step from './step'
+
+const BSComEng = () => {
+  return (
+
+<section class="text-gray-600 body-font overflow-hidden">
+  <div class="container px-6 py-24 mx-auto">
+    <div class="flex flex-col text-center w-full mb-20">
+    <h2 class="text-4xl font-extrabold dark:text-black">Requirements</h2>
+      <p class="lg:w-2/3 mx-auto leading-relaxed text-base text-gray-500">Online and Onsite Requirements.</p>
+      <div class="flex mx-auto border-2 border-red-500 rounded overflow-hidden mt-6">
+        <button class="py-1 px-4 bg-red-500 text-gray-600 focus:outline-none hover:bg-white">FRESHMEN</button>
+        <button class="py-1 px-4 focus:outline-none hover:bg-red-600">TRANSFEREES</button>
+      </div>
+    </div>
+    <div className="flex flex-wrap -m-4 place-content-center">
+        <div class="p-4 xl:w-1/4 md:w-1/2 w-full">
+            <div class="p-6 rounded-lg border-2 border-red-500 flex flex-col relative overflow-hidden">
+            <button class="flex items-center mt-auto text-xl font-extrabold dark:text-white bg-red-500 border-0 py-3 px-5 w-full focus:outline-none hover:bg-red-600 rounded mb-8">FOR FRESHMEN
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-auto" viewBox="0 0 24 24">
+                </svg>
+            </button>
+          
+            <p class="flex items-center text-gray-600 mb-5">
+                <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                    <path d="M20 6L9 17l-5-5"></path>
+                </svg>
+                </span>ORIGINAL FORM 138
+            </p>
+            <p class="flex items-center text-gray-600 mb-5">
+                <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                    <path d="M20 6L9 17l-5-5"></path>
+                </svg>
+                </span>ORIGINAL CERTIFICATE OF GOOD MORAL CHARACTER
+            </p>
+            <p class="flex items-center text-gray-600 mb-5">
+                <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                    <path d="M20 6L9 17l-5-5"></path>
+                </svg>
+                </span>PHOTOCOPY OF PSA AUTHENTICATED BIRTH CERTIFICATE
+            </p>
+            <p class="flex items-center text-gray-600 mb-5">
+                <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                    <path d="M20 6L9 17l-5-5"></path>
+                </svg>
+                </span>TWO PEICES EACH OF"2X2" AND "1X1" ID PICTURE
+            </p>
+            <p class="flex items-center text-gray-600 mb-5">
+                <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                    <path d="M20 6L9 17l-5-5"></path>
+                </svg>
+                </span>TWO PEICES EACH OF"2X2" AND "1X1" ID PICTURE
+            </p>
+            </div>
+        </div>
+        
+        <div class="p-4 xl:w-1/4 md:w-1/2 w-full">
+            <div class="p-6 rounded-lg border-2 border-red-500 flex flex-col relative overflow-hidden">
+            <button class="flex items-center mt-auto text-xl font-extrabold dark:text-white bg-red-500 border-0 py-3 px-4 w-full focus:outline-none hover:bg-red-600 rounded mb-6">FOR TRANSFEREE
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-auto" viewBox="0 0 24 24">
+                </svg>
+            </button>
+            <p class="flex items-center text-gray-600 mb-2">
+                <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                    <path d="M20 6L9 17l-5-5"></path>
+                </svg>
+                </span>ORIGINAL TRANSCRIPT OF RECORD (TOR)
+            </p>
+            <p class="flex items-center text-gray-600 mb-2">
+                <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                    <path d="M20 6L9 17l-5-5"></path>
+                </svg>
+                </span>ORIGINAL CERTIFICATE OF GOOD MORAL CHARACTER
+            </p>
+            <p class="flex items-center text-gray-600 mb-2">
+                <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                    <path d="M20 6L9 17l-5-5"></path>
+                </svg>
+                </span>ORIGINAL HONORABLE DISMISSAL
+            </p>
+            <p class="flex items-center text-gray-600 mb-2">
+                <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                    <path d="M20 6L9 17l-5-5"></path>
+                </svg>
+                </span>PHOTOCOPY OF PSA AUTHENTICATED BIRTH CERTIFICATE
+            </p>
+            <p class="flex items-center text-gray-600 mb-2">
+                <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                    <path d="M20 6L9 17l-5-5"></path>
+                </svg>
+                </span>TWO PEICES EACH OF"2X2" AND "1X1" ID PICTURE
+            </p>
+            <p class="flex items-center text-gray-600 mb-6">
+                <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                    <path d="M20 6L9 17l-5-5"></path>
+                </svg>
+                </span>PHOTOCOPY OF MARRIAGE CERTIFICATE IF MARRIED
+            </p>
+            </div>
+        </div>
+      
+      
+    </div>
+  </div>
+</section>
+
+  )
+}
+
+export default BSComEng
