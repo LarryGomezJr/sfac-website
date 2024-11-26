401360caf0d0c7f9721c5707f95bd676d672be3a
John Rae G. Manuel <garciajohnmanuel016@gmail.com>
2024-11-26 19:39:25 +0800

    Updated Navbar

--- src/pages/components/Navbar.jsx
+++ src/pages/components/Navbar.jsx
@@ -50,7 +50,7 @@ const Navbar = () => {
   const navItems = [
     { name: "Home", href: "/" },
     { name: "About", href: "/about" },
-    { name: "Contact Us", href: "/contact" },
+    { name: "Contact Us", href: "/components/Directory" },
     {
       name: "College Inquiries",
       href: "#",
