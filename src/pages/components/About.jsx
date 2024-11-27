7823d7d8d88387cc821127e98e7a47a806190595
Charlie <charlietorres09566701327@gmail.com>
2024-11-26 16:27:34 -0800

    About-us

--- src/pages/components/About.jsx
+++ src/pages/components/About.jsx
@@ -15,7 +15,7 @@ const About = () => {
                 <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
               </svg>
             </div>
-            <h2 class="text-red-600 text-lg title-font font-medium">VISION</h2>
+            <h2 class="text-red-600 text-xl title-font font-medium">VISION</h2>
           </div>
           <div class="flex-grow">
             <p class="leading-relaxed text-base text-gray-600 text-lg title-font font-sm">Saint Francis of Assisi College (SFAC) is a hub for progressive and holistic education, guided by the virtues of Saint Francis of Assisi that empowers learners with a solid academic foundation enriched by physical, spiritual, and cultural advancement.</p>
@@ -31,7 +31,7 @@ const About = () => {
                 <circle cx="12" cy="7" r="4"></circle>
               </svg>
             </div>
-            <h2 class="text-red-600 text-lg title-font text-base font-medium">MISSION</h2>
+            <h2 class="text-red-600 text-xl title-font text-base font-medium">MISSION</h2>
           </div>
           <div class="flex-grow">
             
@@ -54,15 +54,23 @@ const About = () => {
                 <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
               </svg>
             </div>
-            <h2 class="text-red-600 text-lg title-font font-medium">CORE VALUES</h2>
+            <h2 class="text-red-600 text-xl title-font font-medium">CORE VALUES</h2>
           </div>
           <div class="flex-grow ">
 
             <div class="flex-grow text-gray-900 text-lg title-font font-sm mb-5">SFAC is guided by these core values:</div>
-            <div class="flex-grow text-gray-900 leading-relaxed text-base">Commitment there is a commitment by each and every member of the Franciscan community to the vision set forth by the Institution.</div>
-            <div class="flex-grow text-gray-900 leading-relaxed text-base">Opportunity there is a constant desire to seize the opportunity to become better members and leaders of the community, as well as contributors to nation- building.</div>
-            <div class="flex-grow text-gray-900 leading-relaxed text-base">Respect there is an utmost respect for one's self, peers, and co-stakeholders in the Franciscan community.</div>
-            <div class="flex-grow text-gray-900 leading-relaxed text-base">Excellence there is a drive to always make effective use of talents and skills in order to produce outputs of the highest quality.</div>
+            <div class="flex-grow text-gray-900 leading-relaxed text-base">
+              <span className="bg-gradient-to-r text-red-600 text-lg font-bold bg-clip-text">Commitment </span>
+               there is a commitment by each and every member of the Franciscan community to the vision set forth by the Institution.</div>
+            <div class="flex-grow text-gray-900 leading-relaxed text-base">
+              <span className="bg-gradient-to-r text-red-600 text-lg font-bold bg-clip-text">Opportunity </span>
+               there is a constant desire to seize the opportunity to become better members and leaders of the community, as well as contributors to nation- building.</div>
+            <div class="flex-grow text-gray-900 leading-relaxed text-base">
+            <span className="bg-gradient-to-r text-red-600 text-lg font-bold bg-clip-text">Respect </span>
+               there is an utmost respect for one's self, peers, and co-stakeholders in the Franciscan community.</div>
+            <div class="flex-grow text-gray-900 leading-relaxed text-base">
+            <span className="bg-gradient-to-r text-red-600 text-lg font-bold bg-clip-text">Excellence </span>
+               there is a drive to always make effective use of talents and skills in order to produce outputs of the highest quality.</div>
           </div>
         </div>
       </div>
