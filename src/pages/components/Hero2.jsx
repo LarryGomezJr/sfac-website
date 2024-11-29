ce547383806daad48ede5dde05054fc427e637fa
John Rae G. Manuel <garciajohnmanuel016@gmail.com>
2024-11-29 18:58:31 +0800

    Update Hero2.jsx

--- src/pages/components/Hero2.jsx
+++ src/pages/components/Hero2.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import basicEd from '../../../public/basicEd.jpg';
 import higherEd from '../../../public/higherEd.jpg';
+import Link from 'next/link';
 
 const Hero2 = () => {
   return (
@@ -19,9 +20,11 @@ const Hero2 = () => {
               <h2 className="title-font text-2xl font-medium text-gray-900 mt-6 mb-3">
                 BASIC EDUCATION DEPARTMENT
               </h2>
-              <button className="flex mx-auto mt-6 text-white bg-red-700 border-0 py-2 px-5 focus:outline-none hover:bg-red-300 rounded">
-                Button
-              </button>
+              <div>
+                <Link href="https://www.facebook.com/mysfaclp" target="_blank">
+                <button className="flex mx-auto mt-6 text-white bg-red-700 border-0 py-2 px-5 focus:outline-none hover:bg-red-300 rounded">Click here</button>
+                </Link>
+              </div>
             </div>
             <div className="sm:w-1/2 mb-10 px-2">
               <div className="rounded-lg h-[400px] overflow-hidden"> 
@@ -34,9 +37,11 @@ const Hero2 = () => {
               <h2 className="title-font text-2xl font-medium text-gray-900 mt-6 mb-3">
                 HIGHER EDUCATION DEPARTMENT
               </h2>
-              <button className="flex mx-auto mt-6 text-white bg-red-700 border-0 py-2 px-5 focus:outline-none hover:bg-red-300 rounded">
-                Button
-              </button>
+              <div>
+                <Link href="https://www.facebook.com/@SFACCollegeLasPinas/" target="_blank">
+                <button className="flex mx-auto mt-6 text-white bg-red-700 border-0 py-2 px-5 focus:outline-none hover:bg-red-300 rounded">Click here</button>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
