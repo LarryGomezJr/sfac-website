531334975c61c53a5c0478312a673469672b2bc4
Charlie <charlietorres09566701327@gmail.com>
2024-11-29 17:53:27 -0800

    about

--- src/pages/components/HigherEduc.jsx
+++ src/pages/components/HigherEduc.jsx
@@ -5,16 +5,16 @@ const HigherEduc = () => {
   return (
     <div class="text-gray-600 body-font overflow-hidden">
   <div class="container px-1 py-24 mx-auto">
-    <div class="flex flex-col text-center w-full mb-20">
+    <div class="flex flex-col text-center w-full mb-5">
       <h1 class="sm:text-4xl text-3xl font-medium title-font mb-2 text-gray-900">
         <p class="text-red-600 text-3xl font-bold">OFFERED</p>
         <p class="text-black text-5xl font-bold font-large"> PROGRAMS</p>
       </h1>
     </div>
-    <div class="flex flex-wrap mx-auto place-content-center  -m-4">
-    <div class="p-1 xl:w-1/4 md:w-1/2 w-full">
-        <div class="h-full p-6 rounded-lg border-2 border-gray-700 flex flex-col relative overflow-hidden group block rounded-lg p-6 bg-white ring-1 ring-slate-900 shadow-lg space-y-3 hover:bg-red-600 hover:ring-sky-500">
-        <button class="flex items-center mt-auto font-bold text-white bg-red-600 border-0 py-2 px-4 w-full focus:outline-none mb-5 text-xl rounded group-hover:text-black">COLLEGIATE PROGRAMS
+    <div class="flex flex-wrap mx-auto place-content-center -m-4">
+    <div class="p-3 py-20 xl:w-1/4 md:w-1/2 w-full">
+        <div class="p-4 rounded-lg border-2 border-gray-700 flex flex-col relative overflow-hidden group block rounded-lg bg-white ring-1 ring-slate-900 shadow-lg space-y-3 hover:bg-red-600 hover:ring-sky-500">
+        <button class="flex items-center font-bold text-white bg-red-600 border-0 py-2 px-4 w-full focus:outline-none text-xl rounded group-hover:text-black">COLLEGIATE PROGRAMS
             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-auto" viewBox="0 0 24 24">
               <path d="M5 12h14M12 5l7 7-7 7"></path>
             </svg>
@@ -91,29 +91,29 @@ const HigherEduc = () => {
           </p>
         </div>
       </div>
-      <div class="p-1 xl:w-1/4 md:w-1/2 w-full">
-      <div class="h-full p-6 rounded-lg border-2 border-gray-700 flex flex-col relative overflow-hidden group block rounded-lg p-6 py-20 bg-white ring-1 ring-slate-900 shadow-lg space-y-3 hover:bg-red-600 hover:ring-sky-500">
-        <button class="flex items-center font-bold mt-auto text-white bg-red-600 border-0 py-2 px-4 w-full focus:outline-none mb-20 text-xl rounded group-hover:text-black">GRADUATE PROGRAMS
+      <div class="p-3 py-20 xl:w-1/4 md:w-1/2 w-full">
+      <div class="p-4 rounded-lg border-2 border-gray-700 flex flex-col relative overflow-hidden group block rounded-lg bg-white ring-1 ring-slate-900 shadow-lg space-y-3 hover:bg-red-600 hover:ring-sky-500">
+        <button class="flex items-center font-bold text-white bg-red-600 border-0 py-2 px-4 w-full focus:outline-none text-xl rounded group-hover:text-black">GRADUATE PROGRAMS
             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-auto" viewBox="0 0 24 24">
               <path d="M5 12h14M12 5l7 7-7 7"></path>
             </svg>
         </button>
           <p class="text-lg text-gray-600 mb-2 group-hover:text-white">
-            <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-red-600 text-white rounded-full flex-shrink-0 group-hover:text-black mb-20 mt-14 ">
+            <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-red-600 text-white rounded-full flex-shrink-0 group-hover:text-black">
               <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
                 <path d="M20 6L9 17l-5-5"></path>
               </svg>
             </span>Master In Business Administration
           </p>
           <p class="text-lg text-gray-600 mb-20 group-hover:text-white">
-            <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-red-600 text-white rounded-full flex-shrink-0 group-hover:text-black mb-20 mt-20">
+            <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-red-600 text-white rounded-full flex-shrink-0 group-hover:text-black">
               <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
                 <path d="M20 6L9 17l-5-5"></path>
               </svg>
             </span>Master of Arts in Education
           </p>
           <p class="text-lg text-gray-600 mb-2 group-hover:text-white">
-            <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-red-600 text-white rounded-full flex-shrink-0 group-hover:text-black mb-20 mt-20">
+            <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-red-600 text-white rounded-full flex-shrink-0 group-hover:text-black">
               <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
                 <path d="M20 6L9 17l-5-5"></path>
               </svg>
