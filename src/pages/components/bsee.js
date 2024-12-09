da08f6f5119013ad9ef151545f41d2c962ef9c74
AkosiBatm4n <yanyan.another.acc0unt@gmail.com>
2024-12-09 19:48:06 +0800

    UPDATE: PAYMENTS AND STEPS

--- src/pages/components/bsee.js
+++ src/pages/components/bsee.js
@@ -14,8 +14,7 @@ const bsee = () => {
 
   
 <section class="text-gray-600 body-font">
-  <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
-    <Image class="lg:w-2/6 :w-3/6 w-5/6 mb-10 object-cover object-center rounded" width={1500} height={500} alt="hero" src="/sfaclogo.png"/>
+  <div class="container mx-auto flex px-5 py-28 items-center justify-center flex-col">
     <div class="text-center lg:w-2/3 w-full">
       <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">BACHELOR OF SCIENCE IN ELECTRICAL ENGINEERING</h1>
       <p class="mb-8 leading-relaxed">Guided by the virtues of Saint Francis of Assisi Our future engineers ensure a brighter future for generations to come. Below serves as your starting point for your journey.</p>
@@ -26,7 +25,7 @@ const bsee = () => {
 {/* REQUIREMENTS TO */}
 
 <section class="text-gray-600 body-font overflow-hidden">
-  <div class="container px-5 py-24 mx-auto">
+  <div class="container px-5 py-2 mx-auto">
     <div class="flex flex-col text-center w-full mb-20">
       <h1 class="sm:text-4xl text-3xl font-medium title-font mb-2 text-gray-900">REQUIREMENTS</h1>
     </div>
@@ -78,48 +77,70 @@ const bsee = () => {
           <h1 class="text-3xl text-gray-900 leading-none flex items-center pb-4 mb-4 border-b border-gray-200">
             <span>TRANSFEREES</span>
           </h1>
-          <p class="flex items-center text-gray-600 mb-2">
+          <div class="flex items-center text-gray-600 mb-2">
             <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-red-600 text-white rounded-full flex-shrink-0">
               <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
                 <path d="M20 6L9 17l-5-5"></path>
               </svg>
             </span>- ORIGINAL TRANSCRIPT OF RECORD (TOR)
-          </p>
-          <p class="flex items-center text-gray-600 mb-2">
+          </div>
+          <div class="flex items-center text-gray-600 mb-2">
             <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-red-600 text-white rounded-full flex-shrink-0">
               <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
                 <path d="M20 6L9 17l-5-5"></path>
               </svg>
             </span>- ORIGINAL CERTIFICATE OF GOOD MORAL CHARACTER
-          </p>
-          <p class="flex items-center text-gray-600 mb-2">
+          </div>
+          <div class="flex items-center text-gray-600 mb-2">
             <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-red-600 text-white rounded-full flex-shrink-0">
               <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
                 <path d="M20 6L9 17l-5-5"></path>
               </svg>
             </span>- ORIGINAL HONORABLE DISMISSAL
-          </p>
-          <p class="flex items-center text-gray-600 mb-2">
+          </div>
+          <div class="flex items-center text-gray-600 mb-2">
             <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-red-600 text-white rounded-full flex-shrink-0">
               <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
                 <path d="M20 6L9 17l-5-5"></path>
               </svg>
             </span>- PHOTOCOPY OF PSA AUTHENTICATED BIRTH CERTIFICATE
-          </p>
-          <p class="flex items-center text-gray-600 mb-2">
+          </div>
+          <div class="flex items-center text-gray-600 mb-2">
             <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-red-600 text-white rounded-full flex-shrink-0">
               <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
                 <path d="M20 6L9 17l-5-5"></path>
               </svg>
             </span>- TWO PIECES EACH OF "2X2" AND "1X1" ID PICTURE
-          </p>
-          <p class="flex items-center text-gray-600 mb-2">
+          </div>
+          <div class="flex items-center text-gray-600 mb-2">
             <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-red-600 text-white rounded-full flex-shrink-0">
               <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
                 <path d="M20 6L9 17l-5-5"></path>
               </svg>
             </span>- PHOTOCOPY OF MARRIAGE CERTIFICATE IF MARRIED
-          </p>
+          </div>
+          <div class="flex items-center text-gray-600 mb-2 text-justify">
+            <strong>
+              "*Note: Transferee students should be under evaluation first. Should you wish to know the subjects that can be credited or taken, please email titsermardz@gmail.com and sfaccollegeregistrar@yahoo.com.ph with your copy of grades taken or TOR attached."
+            </strong>
+          </div>
+          <br/>
+            <br/>
+            <h1>
+            Indicate Name:
+            </h1>
+            <br/>
+            <h2>
+            Contact number:
+            </h2>
+            <br/>
+            <h3>
+            Preferred course to take in SFAC:
+            </h3>
+            <br/>
+            <h4>
+            Previous course:
+            </h4>
         </div>
       </div>
     </div>
@@ -129,7 +150,10 @@ const bsee = () => {
 {/* STEPS TO */}
 
 <section class="text-gray-600 body-font">
-  <div class="container px-5 py-24 mx-auto flex flex-wrap">
+  <div class="container px-5 py-10 mx-auto flex flex-wrap">
+  <div class="flex flex-col text-center w-full mb-20">
+      <h1 class="sm:text-4xl text-3xl font-medium title-font mb-2 text-gray-900">STEPS</h1>
+    </div>
     <div class="flex flex-wrap w-full">
       <div class="lg:w-2/5 md:w-1/2 md:pr-10 md:py-6">
         <div class="flex relative pb-12">
@@ -209,69 +233,70 @@ const bsee = () => {
 <section class="text-gray-600 body-font">
   <div class="container px-5 py-24 mx-auto">
   <div class="flex flex-col text-center w-full mb-20">
-      <h1 class="sm:text-4xl text-3xl font-medium title-font mb-2 text-gray-900">PAYMENTS GAGI</h1>
+      <h1 class="sm:text-4xl text-3xl font-medium title-font mb-2 text-gray-900">PAYMENT OPTIONS</h1>
     </div>
-    <div class="flex flex-wrap -m-4">
-      <div class="p-4 lg:w-1/3">
-        <div class="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
-          <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
-          <h1 class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">Raclette Blueberry Nextious Level</h1>
-          <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-
-          <div class="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4">
-            <span class="text-gray-400 mr-3 inline-flex items-center leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
-              <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-                <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                <circle cx="12" cy="12" r="3"></circle>
-              </svg>1.2K
-            </span>
-            <span class="text-gray-400 inline-flex items-center leading-none text-sm">
-              <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-                <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-              </svg>6
-            </span>
-          </div>
+    <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
+      <div class="p-4 md:w-1/3 sm:mb-0 mb-6">
+        <div class="rounded-lg h-54 overflow-hidden">
+          <Image alt="content" class="object-cover object-center h-full w-full" width={1500} height={1500}src="/eastwest.jpg"/>
+        </div>
+        <h2 class="text-xl font-medium title-font text-gray-900 mt-5">OPTION 1 - BANK DEPOSIT</h2>
+        <div class="text-base leading-relaxed mt-2">EAST WEST BANK</div>
+        <div><br/>
+            <h1>
+            Payment Details
+            </h1>
+            <br/>
+            <h1>
+            Account Name: Saint Francis de Assisi Educational Foundation Inc.
+            </h1>
+            <br/>
+            <h1>
+            Acoount Number: 2000-0074-2383
+            </h1>
+            <br/>
+            <h1>
+            Branch: Alabang-Madrigal
+            </h1>
         </div>
       </div>
-      <div class="p-4 lg:w-1/3">
-        <div class="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
-          <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
-          <h1 class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">Ennui Snackwave Thundercats</h1>
-          <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-          <div class="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4">
-            <span class="text-gray-400 mr-3 inline-flex items-center leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
-              <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-                <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                <circle cx="12" cy="12" r="3"></circle>
-              </svg>1.2K
-            </span>
-            <span class="text-gray-400 inline-flex items-center leading-none text-sm">
-              <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-                <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-              </svg>6
-            </span>
-          </div>
+      <div class="p-4 md:w-1/3 sm:mb-0 mb-6">
+        <div class="rounded-lg h-54 overflow-hidden">
+          <Image alt="content" class="object-cover object-center h-full w-full" src="/GCash.png" width={1500} height={1500}/>
+        </div>
+        <h2 class="text-xl font-medium title-font text-gray-900 mt-5">OPTION 2 - G-CASH PAYMENT</h2>
+        <div><br/>
+            <h1>
+            STEP 1 - Select Bank Transfer
+            </h1>
+            <br/>
+            <h1>
+            STEP 2 - Select Eastwest Bank Corporation
+            </h1>
+            <br/>
+            <h1>
+            STEP 3:
+            </h1>
+            <br/>
+            <h1>
+           ENTER AMOUNT
+            </h1>
+            <br/>
+            <h1>
+          ACCOUNT NAME
+            </h1>
+            <br/>
+            <h1>
+          ACCOUNT NUMBER
+            </h1>
         </div>
       </div>
-      <div class="p-4 lg:w-1/3">
-        <div class="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
-          <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
-          <h1 class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">Selvage Poke Waistcoat Godard</h1>
-          <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-          <div class="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4">
-            <span class="text-gray-400 mr-3 inline-flex items-center leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
-              <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-                <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                <circle cx="12" cy="12" r="3"></circle>
-              </svg>1.2K
-            </span>
-            <span class="text-gray-400 inline-flex items-center leading-none text-sm">
-              <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-                <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-              </svg>6
-            </span>
-          </div>
+      <div class="p-4 md:w-1/3 sm:mb-0 mb-6">
+        <div class="rounded-lg h-54 overflow-hidden place-items-center" >
+          <Image alt="content" class="object-cover object-center h-full w-full" width={500} height={500} src="/sfaclogo.png"/>
         </div>
+        <h2 class="text-xl font-medium title-font text-gray-900 mt-5">OPTION 3 - DIRECT PAYMENT</h2>
+        <Link href="https://maps.app.goo.gl/PFXtRt73hTJr8Y2x8" target="_blank" className="text-red-500"> CLICK ME FOR DIRECTIONS!</Link>
       </div>
     </div>
   </div>
