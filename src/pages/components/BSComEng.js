9b6413f656615e79e368e848850c6049c3f248fe
nitchiii <148184975+nitchiii@users.noreply.github.com>
2024-12-09 19:50:23 +0800

    UPDATED

--- src/pages/components/BSComEng.js
+++ src/pages/components/BSComEng.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import Step from './Step'
+import Image from 'next/image'
 
 const BSComEng = () => {
   return (
+
 
 <section class="text-gray-600 body-font overflow-hidden">
   <div class="container px-6 py-24 mx-auto">
@@ -10,14 +12,14 @@ const BSComEng = () => {
     <h2 class="text-4xl font-extrabold dark:text-black">Requirements</h2>
       <p class="lg:w-2/3 mx-auto leading-relaxed text-base text-gray-500">Online and Onsite Requirements.</p>
       <div class="flex mx-auto border-2 border-red-500 rounded overflow-hidden mt-6">
-        <button class="py-1 px-4 bg-red-500 dark:text-white focus:outline-none text-lg">FRESHMEN</button>
+        <button class="py-1 px-4 bg-red-500 text-white focus:outline-none text-lg">FRESHMEN</button>
         <button class="py-1 px-4 focus:outline-none text-lg">TRANSFEREES</button>
       </div>
     </div>
     <div className="flex flex-wrap -m-4 place-content-center">
         <div class="p-4 xl:w-1/4 md:w-1/2 w-full">
             <div class="p-6 rounded-lg border-2 border-red-500 flex flex-col relative overflow-hidden">
-            <button class="flex items-center mt-auto text-xl font-extrabold dark:text-white bg-red-500 border-0 py-3 px-5 w-full focus:outline-none hover:bg-red-600 rounded mb-8">FOR FRESHMEN
+            <button class="flex items-center mt-auto text-xl font-extrabold text-white bg-red-500 border-0 py-3 px-5 w-full focus:outline-none hover:bg-red-600 rounded mb-8">FOR FRESHMEN
                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-auto" viewBox="0 0 24 24">
                 </svg>
             </button>
@@ -62,7 +64,7 @@ const BSComEng = () => {
         
         <div class="p-4 xl:w-1/4 md:w-1/2 w-full">
           <div class="p-6 rounded-lg border-2 border-red-500 flex flex-col relative overflow-hidden">
-            <button class="flex items-center mt-auto text-xl font-extrabold dark:text-white bg-red-500 border-0 py-3 px-4 w-full focus:outline-none hover:bg-red-600 rounded mb-6">FOR TRANSFEREE
+            <button class="flex items-center mt-auto text-xl font-extrabold text-white bg-red-500 border-0 py-3 px-4 w-full focus:outline-none hover:bg-red-600 rounded mb-6">FOR TRANSFEREE
                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-auto" viewBox="0 0 24 24">
                 </svg>
             </button>
@@ -99,7 +101,7 @@ const BSComEng = () => {
                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
                     <path d="M20 6L9 17l-5-5"></path>
                 </svg>
-                </span>TWO PiECES EACH OF"2X2" AND "1X1" ID PICTURE
+                </span>TWO PIECES EACH OF"2X2" AND "1X1" ID PICTURE
             </p>
             <p class="flex items-center text-gray-600 mb-6">
                 <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-red-600 text-white rounded-full flex-shrink-0">
--- src/pages/components/BSEDUC.js
+++ src/pages/components/BSEDUC.js
@@ -10,14 +10,14 @@ const BSEDUC = () => {
     <h2 class="text-4xl font-extrabold dark:text-black">Requirements</h2>
       <p class="lg:w-2/3 mx-auto leading-relaxed text-base text-gray-500">Online and Onsite Requirements.</p>
       <div class="flex mx-auto border-2 border-red-500 rounded overflow-hidden mt-6">
-        <button class="py-1 px-4 bg-red-500 dark:text-white focus:outline-none text-lg">FRESHMEN</button>
+        <button class="py-1 px-4 bg-red-500 text-white focus:outline-none text-lg">FRESHMEN</button>
         <button class="py-1 px-4 focus:outline-none text-lg">TRANSFEREES</button>
       </div>
     </div>
     <div className="flex flex-wrap -m-4 place-content-center">
         <div class="p-4 xl:w-1/4 md:w-1/2 w-full">
             <div class="p-6 rounded-lg border-2 border-red-500 flex flex-col relative overflow-hidden">
-            <button class="flex items-center mt-auto text-xl font-extrabold dark:text-white bg-red-500 border-0 py-3 px-5 w-full focus:outline-none hover:bg-red-600 rounded mb-8">FOR FRESHMEN
+            <button class="flex items-center mt-auto text-xl font-extrabold text-white bg-red-500 border-0 py-3 px-5 w-full focus:outline-none hover:bg-red-600 rounded mb-8">FOR FRESHMEN
                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-auto" viewBox="0 0 24 24">
                 </svg>
             </button>
@@ -62,7 +62,7 @@ const BSEDUC = () => {
         
         <div class="p-4 xl:w-1/4 md:w-1/2 w-full">
           <div class="p-6 rounded-lg border-2 border-red-500 flex flex-col relative overflow-hidden">
-            <button class="flex items-center mt-auto text-xl font-extrabold dark:text-white bg-red-500 border-0 py-3 px-4 w-full focus:outline-none hover:bg-red-600 rounded mb-6">FOR TRANSFEREE
+            <button class="flex items-center mt-auto text-xl font-extrabold text-white bg-red-500 border-0 py-3 px-4 w-full focus:outline-none hover:bg-red-600 rounded mb-6">FOR TRANSFEREE
                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-auto" viewBox="0 0 24 24">
                 </svg>
             </button>
@@ -99,7 +99,7 @@ const BSEDUC = () => {
                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
                     <path d="M20 6L9 17l-5-5"></path>
                 </svg>
-                </span>TWO PiECES EACH OF"2X2" AND "1X1" ID PICTURE
+                </span>TWO PIECES EACH OF"2X2" AND "1X1" ID PICTURE
             </p>
             <p class="flex items-center text-gray-600 mb-6">
                 <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-red-600 text-white rounded-full flex-shrink-0">
